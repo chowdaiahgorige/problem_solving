cf8520613064b615289dfad828baadc1f1f0c58d
chowdaiahgorige <gorigechowdaiah371@gmail.com>
2025-07-26 13:11:52 +0530

    added

---
+++ Dsa_Evaluation/secound_test.js
@@ -0,0 +1,154 @@
+//1.
+//ans:
+// option:d:both b and c
+
+
+
+//2.
+//ans:
+// option:b:"number"
+
+//3.
+//ans:
+// option:a
+
+
+//4.
+//ans:
+// X=12;
+
+
+//5.
+//ans:
+// object is not a primitive data type.Because it is a non-primitive data type.
+
+
+//6.
+//ans:
+// option :A
+
+
+//7.
+//ans:
+// True
+
+
+//8.
+//ans:
+// The else if is used to check one or more conditions
+
+
+//9.
+//ans:
+// "Not an adult"
+
+
+//10.
+//ans:
+// option:B
+
+
+//11.
+//ans:
+// let a="chowdaiah";
+// let b=20;
+// console.log(a)
+// console.log(typeof a)
+// console.log(b)
+// console.log(typeof b)
+
+//12.
+//ans:
+// let x=5;
+// let y=10;
+// let z=x;
+// x=y;
+// y=z;
+// console.log(x,y)
+
+//13.
+//ans:
+// let n=14;
+// if(n%2==0){
+//     console.log("Even")
+// }
+// else{
+//     console.log("Odd")
+// }
+
+//14.
+//ans:
+// let sum=0;
+// for(let i=1;i<=50;i++){
+//     sum=sum+i
+// }
+// console.log(sum)
+
+//15.
+//ans:
+// for (let i = 1; i <= 20; i++) {
+  
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(`${i}X${j}=${i * j}`)
+//   }
+ 
+// }
+
+
+//16.
+//ans:
+// let a=10;
+// let b=20;
+// let c=30;
+// if(a>b && a>c){
+//     console.log("a is bigger")
+// }
+// else if(b>a && b>c){
+//     console.log("b is bigger")
+// }
+// else if(c>a && c>b){
+//     console.log("c is bigger")
+// }
+// else{
+//     console.log("enter a valid number")
+// }
+
+//17.
+//ans:
+// for(let i=10;i>=1;i--){
+//     console.log(i)
+// }
+
+//18.
+//ans:
+// let char="e"
+// if(char=="a"||char=="e" ||char=="i" ||char=="o" ||char=="u"){
+//     console.log("vowel")
+// }
+// else{
+//     console.log("consonent")
+// }
+
+
+//19.
+//ans:
+// let num=4;
+// let a=6;
+// console.log(num += a)
+// console.log(num -= a)
+// console.log(num *= a)
+// console.log(num /= a)
+
+
+//20.
+//ans:
+// let a=1;
+// if(a=="+1"){
+//     console.log("positive")
+// }
+// else if(a=="-1"){
+//     console.log("negative")
+// }
+// else if(a=="0"){
+//     console.log(zero)
+// }
