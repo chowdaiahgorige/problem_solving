e6c41cbb0b9a33deccddaebb54a9559314a8d6f6
chowdaiahgorige <gorigechowdaiah371@gmail.com>
2025-08-01 20:33:29 +0530

    added

---
+++ day12.js
@@ -0,0 +1,113 @@
+// let student={
+//     name:"chowdaiah",
+//     age:22,
+//     grade:"A++"
+// }
+// console.log(student)
+
+
+//2.
+//ans:
+// let student={
+//     name:"chowdaiah",
+//     age:22,
+//     grade:"A++"
+// }
+//  let a=student.name
+// console.log(a)
+
+
+
+//3.
+//ans:
+// let student={
+//     name:"chowdaiah",
+//     age:22,
+//     grade:"A++"
+// }
+// student.gmail="gorige@gmail.com"
+// console.log(student)
+
+
+//4.
+//ans:
+// let student={
+//     name:"chowdaiah",
+//     age:22,
+//     grade:"A++"
+// }
+// student.grade="A+"
+// console.log(student)
+
+
+//5.
+//ans:
+// let student={
+//     name:"chowdaiah",
+//     age:22,
+//     grade:"A++"
+// }
+// delete student.age
+// console.log(student)
+
+//6.
+//ans:
+// let student={
+//    name:"chowdaah",
+//    greet: function(name){
+//         console.log("my name is",this.name)
+//     }
+// }
+// student.greet()
+
+
+
+//8.
+//ans:
+// let obj={
+//     name:"Chowdaiah",
+//     age:21,
+//     gmail:"@gmail.com",
+//     roll:23,
+// }
+// console.log(obj)
+
+
+//9.
+//ans:
+// let obj={
+//     name:"nithin",
+//     age:23,
+//     mobile:"9346835155",
+// }
+// console.log(obj)
+// let a=Object.hasOwnProperty("two")
+// console.log(a)
+
+
+//10.
+//ans:
+// let students = [
+//   {
+//     name: "John Doe",
+//     age: 20,
+//     grade: "A"
+//   },
+//   {
+//     name: "Jane Smith",
+//     age: 21,
+//     grade: "B"
+//   },
+//   {
+//     name: "Bob Johnson",
+//     age: 19,
+//     grade: "A"
+//   }
+// ];
+
+// for (let i = 0; i < students.length; i++) {
+//   console.log(students[i].name);
+// }
+
+
+
